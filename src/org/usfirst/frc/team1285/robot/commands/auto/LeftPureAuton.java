bdb01182cd7aff295327171292ae4f26798a1367
1241Controls <1241controls@gmail.com>
2018-04-13 18:38:51 -0400

    update for hanger change

--- src/org/usfirst/frc/team1285/robot/commands/auto/LeftPureAuton.java
+++ src/org/usfirst/frc/team1285/robot/commands/auto/LeftPureAuton.java
@@ -13,16 +13,12 @@ public class LeftPureAuton extends CommandGroup {
 
     public LeftPureAuton(char first, char second) {
     	if(second == 'L') {
-    		
     		addSequential(new DrivePath(new Point(0,0),new Point(0,50),new Point(-15,150),new Point(-15,320), 3, 1));
-    		addSequential(new ExtendIntakePivot(false));
-    		addSequential(new WaitCommand(1));
     		addParallel(new DriveTurn(65, 1, 1));
-    		
     		addSequential(new RunElevator(NumberConstants.SCALE_TOP,1, 3));
         	addSequential(new DriveDistance(30, 0.6, 70, 2));
         	addSequential(new OpenIntakeClamp(true));
-    	    
+    	    addSequential(new ExtendIntakePivot(false));
     	    
     	    addSequential(new DriveDistance(-30, 0.6, 90, 2));
     	    addSequential(new RunElevator(NumberConstants.GROUND,0.8, 3));
